c176cd039106d992178df7bc60aee1597e9cb157
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-08-06 13:45:07 +0700

    landing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect(route('login'));
 });
+Route::get('landing', [LandingController::class, 'index'])->name('landing');
 
 // is login
 Route::prefix('dashboard')
