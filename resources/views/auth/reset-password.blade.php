30c53400935b122b2fc5b794f33c57b9c8890aed
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-08-07 17:35:37 +0700

    reset password finish

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,56 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
-
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Reset Password') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+@extends('layouts.dark')
+@section('isi_halaman')
+<!--begin::Wrapper-->
+<div class="w-lg-500px bg-white rounded shadow-sm p-10 p-lg-15 mx-auto">
+						<!--begin::Form-->
+							<!--begin::Heading-->
+							<div class="text-center mb-10">
+								<!--begin::Title-->
+								<h1 class="text-dark mb-3">Reset Password</h1>
+								<!--end::Title-->
+								<!--begin::Link-->
+								<div class="text-gray-400 fw-bold fs-4">Enter your new password.</div>
+								<!--end::Link-->
+							</div>
+							<!--begin::Heading-->
+							<!--begin::Input group-->
+							<form method="post" action="{{ route('password.update') }}">
+							    @csrf
+							    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+							    <div class="fv-row mb-10">
+								@if ($errors->any())
+    								<div class="alert alert-danger" role="alert">
+									<p class="fw-bolder text-gray-800 fs-6">Something Went Wrong</p>
+           						 @foreach ($errors->all() as $error)
+									<span style="color: rgb(187, 8, 8)" class="text-mute fw-bold d-block">{{$error}}</span>
+            					@endforeach	
+									</div>
+								@endif
+								</div>
+							<div class="fv-row mb-10">
+								<label class="form-label fw-bolder text-gray-900 fs-6">Email</label>
+								<input class="form-control form-control-solid" type="email" placeholder="" name="email" value="{{request()->input('email')}}" autocomplete="off" readonly/>
+							</div>
+							<div class="fv-row mb-10">
+								<label class="form-label fw-bolder text-gray-900 fs-6">New Password</label>
+								<input class="form-control form-control-solid" type="password" placeholder="" name="password"/>
+							</div>
+							<div class="fv-row mb-10">
+								<label class="form-label fw-bolder text-gray-900 fs-6">Confirm Password</label>
+								<input class="form-control form-control-solid" type="password" placeholder="" name="password_confirmation"/>
+							</div>
+							<!--end::Input group-->
+							<!--begin::Actions-->
+							<div class="d-flex flex-wrap justify-content-center pb-lg-0">
+								<button type="submit" class="btn btn-lg btn-primary fw-bolder me-4">
+						            Submit
+								</button>
+								<a href="{{route('login')}}" class="btn btn-lg btn-light-primary fw-bolder">Cancel</a>
+							</div>
+							<!--end::Actions-->
+						</form>
+						<!--end::Form-->
+					</div>
+					<!--end::Wrapper-->
+@endsection('isi_halaman')
