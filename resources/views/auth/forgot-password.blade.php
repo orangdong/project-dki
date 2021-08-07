3a7848f10965990f4df1742bd126e5842f019b7a
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-08-07 17:36:16 +0700

    forgot password view update

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,7 +3,6 @@
 <!--begin::Wrapper-->
 <div class="w-lg-500px bg-white rounded shadow-sm p-10 p-lg-15 mx-auto">
 						<!--begin::Form-->
-						<form class="form w-100" novalidate="novalidate" id="kt_password_reset_form" method="post" action="">
 							<!--begin::Heading-->
 							<div class="text-center mb-10">
 								<!--begin::Title-->
@@ -15,6 +14,23 @@
 							</div>
 							<!--begin::Heading-->
 							<!--begin::Input group-->
+							<form method="post" action="{{ route('password.email') }}">
+							    @csrf
+							    <div class="fv-row mb-10">
+								@if ($errors->any())
+    								<div class="alert alert-danger" role="alert">
+									<p class="fw-bolder text-gray-800 fs-6">Something Went Wrong</p>
+           						 @foreach ($errors->all() as $error)
+									<span style="color: rgb(187, 8, 8)" class="text-mute fw-bold d-block">{{$error}}</span>
+            					@endforeach	
+									</div>
+								@endif
+								@if (session('status'))
+                                    <div style="font-size: 16px;line-height: 1.25rem;color: rgba(5, 150, 105);font-weight: 500;" class="mb-4">
+										{{ session('status') }}
+									</div>
+                                @endif
+								</div>
 							<div class="fv-row mb-10">
 								<label class="form-label fw-bolder text-gray-900 fs-6">Email</label>
 								<input class="form-control form-control-solid" type="email" placeholder="" name="email" autocomplete="off" />
@@ -22,12 +38,10 @@
 							<!--end::Input group-->
 							<!--begin::Actions-->
 							<div class="d-flex flex-wrap justify-content-center pb-lg-0">
-								<button type="button" id="kt_password_reset_submit" class="btn btn-lg btn-primary fw-bolder me-4">
-									<span class="indicator-label">Submit</span>
-									<span class="indicator-progress">Please wait...
-									<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+								<button type="submit" class="btn btn-lg btn-primary fw-bolder me-4">
+						            Submit
 								</button>
-								<a href="authentication/flows/dark/sign-up.html" class="btn btn-lg btn-light-primary fw-bolder">Cancel</a>
+								<a href="{{route('login')}}" class="btn btn-lg btn-light-primary fw-bolder">Cancel</a>
 							</div>
 							<!--end::Actions-->
 						</form>
